866427237af11e2439e4e20dc60cc638dfc9e816
LimRi <57161327+rigo0523@users.noreply.github.com>
2021-10-19 23:27:05 -0500

    Update posts.js

--- __tests__/posts.js
+++ __tests__/posts.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const server = require("../api/server");
 
 describe("GET / members integrations test", () => {
-  it("GET list of members /api/members", async () => {
+  it("GET list of members /api/posts", async () => {
     const res = await request(server).get("/api/posts");
     expect(res.statusCode).toBe(400);
     expect(res.type).toBe("application/json");
